df9b2e5e6b4e8c3648adc8f81b678e3ee24fffbf
Danny Su <contact@dannysu.com>
2019-04-28 00:04:57 -0700

    Add x-api-key validation

--- src/mercury-parser.js
+++ src/mercury-parser.js
@@ -2,8 +2,26 @@ import Mercury from '@postlight/mercury-parser';
 
 import { corsSuccessResponse, corsErrorResponse, runWarm } from './utils';
 
-const mercuryParser = async ({ queryStringParameters }, context, cb) => {
+const mercuryParser = async (
+  { queryStringParameters, headers },
+  context,
+  cb
+) => {
+  if (!queryStringParameters) {
+    return cb(
+      null,
+      corsErrorResponse({
+        message: 'Provide an URL in the query string: ?url=',
+      })
+    );
+  }
   const { url } = queryStringParameters;
+  if (
+    !('x-api-key' in headers) ||
+    headers['x-api-key'] !== process.env.SECRET_TOKEN
+  ) {
+    return cb(null, corsErrorResponse({ message: 'Invalid x-api-key' }));
+  }
 
   const result = await Mercury.parse(url);
 
--- src/parse-html.js
+++ src/parse-html.js
@@ -2,8 +2,14 @@ import Mercury from '@postlight/mercury-parser';
 
 import { corsSuccessResponse, corsErrorResponse, runWarm } from './utils';
 
-const parseHtml = async ({ body }, context, cb) => {
+const parseHtml = async ({ body, headers }, context, cb) => {
   const { url, html } = JSON.parse(body);
+  if (
+    !('x-api-key' in headers) ||
+    headers['x-api-key'] !== process.env.SECRET_TOKEN
+  ) {
+    return cb(null, corsErrorResponse({ message: 'Invalid x-api-key' }));
+  }
 
   const result = await Mercury.parse(url, { html });
 
